867f60a92c0c85bc5552e9205a9624e16aa59f15
Vishnu <82367813+tren03@users.noreply.github.com>
2026-09-08 11:29:56 +0530

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,33 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+# 33 mins
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        starts = sorted([i.start for i in intervals])
+        ends = sorted([i.end for i in intervals])
+        print(starts,ends)
+        s = 0
+        e = 0
+        ans = 0
+        count = 0
+        while e < len(intervals):
+            cur_end = ends[e]
+            if s < len(intervals) and starts[s] < cur_end:
+                # means that, there is a concurrent conflict
+                count += 1
+                s += 1
+                ans = max(ans, count)
+                continue
+            count -= 1
+            e += 1
+        return ans
+
+
+
+            
