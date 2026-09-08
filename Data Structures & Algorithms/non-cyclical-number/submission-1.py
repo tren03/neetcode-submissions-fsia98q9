4dabf50940a63c7d58e93aeb3149317ca818dbcf
Vishnu <82367813+tren03@users.noreply.github.com>
2026-09-08 21:48:11 +0530

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """
+        123 
+        123 % 10 = 3
+        123 / 10 = 12
+        """
+        seen = set()
+        while True:
+            s = 0
+            while n > 0:
+                print(n)
+                digit = n % 10
+                n = n // 10
+                s += digit * digit
+            if s == 1:
+                return True
+            if s in seen:
+                return False
+            seen.add(s)
+            n = s
+
+
+
+
+        
